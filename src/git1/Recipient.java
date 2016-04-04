05724a2acaabddb90b220f22b5a0ff4253ceb6aa
fcd <fcd@WIN-O4SV84MG8NT>
2016-04-04 17:32:53 +0200

    modif class Recipient

--- src/git1/Recipient.java
+++ src/git1/Recipient.java
@@ -9,42 +9,66 @@ package git1;
  *
  * @author bphou
  */
-class Recipient {
-
-    void setQuantite(int i) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+public class Recipient {
+    private int quantite;
+    private int capacite;
+    private boolean remplis;
+    
+    public Recipient(){
+        quantite = 0;
+        capacite = 100;
+        remplis = false;
     }
-
-    void remplir(Recipient reservoir) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void viderDans(Recipient reservoir) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    int getQuantite() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void mettreUnDans(Recipient reservoir) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    int getCapacite() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    int getEspaceLibre() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    boolean estVide() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    boolean estPlein() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Recipient(int quantite){
+        this.quantite = quantite;
+        capacite = 100;
+        if (capacite == quantite)
+            remplis = true;
+        else remplis = false;
     }
     
+    public int getQuantite(){
+        return quantite;
+    }
+    public int getCapacite(){
+        return capacite;
+    }
+    public boolean getRemplis(){
+        return remplis;
+    }
+    public void setQuantite(int quantite){
+        this.quantite = quantite;
+    }
+    public void setRemplis(boolean remplis){
+        this.remplis = remplis;
+    }
+    
+    public void mettreUnDans(Recipient autre){
+        if (autre != null && quantite > 0 && autre.getQuantite() < autre.getCapacite()){
+            quantite -= 1;
+            remplis = false;
+            autre.setQuantite(autre.getQuantite() + 1);
+            if (autre.getQuantite() == autre.getCapacite())
+                autre.setRemplis(true);
+            else autre.setRemplis(false);
+        }
+    }
+    public void remplir(Recipient autre){
+        if (autre != null && autre.getRemplis() != true && quantite >= (autre.getCapacite() - autre.getQuantite())){
+            quantite -= (autre.getCapacite() - autre.getQuantite());
+            remplis = false;
+            autre.setQuantite(autre.getCapacite());
+            autre.setRemplis(true);
+        }
+    }
+    public void viderDans(Recipient autre){
+        if (autre != null && quantite > 0 && quantite <= (autre.getCapacite() - autre.getQuantite())){
+            autre.setQuantite(autre.getQuantite() + quantite);
+            if (autre.getQuantite() == autre.getCapacite())
+                autre.setRemplis(true);
+            else autre.setRemplis(false);
+            quantite = 0;
+            remplis = false;
+        }
+    }
 }
